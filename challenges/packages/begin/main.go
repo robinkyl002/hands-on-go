50a5ef8308949c546a79d2e15524be5d7a6bbce4
robinkyl002 <112645678+robinkyl002@users.noreply.github.com>
2024-02-10 22:04:33 +0000

    Finished the first challenge

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -2,9 +2,20 @@
 package main
 
 // import the proverbs package
+import (
+	"fmt"
+
+	"github.com/jboursiquot/go-proverbs"
+)
 
 // getRandomProverb returns a random proverb from the proverbs package
 
+func getRandomProverb() (proverb string) {
+	proverb = proverbs.Random().Saying
+	return
+}
+
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomProverb())
 }
