8f8ea853ee28157d2a9c25535d2cb7c257c6ba1e
robinkyl002 <112645678+robinkyl002@users.noreply.github.com>
2024-02-10 21:51:34 +0000

    Playing with functions with different return types

--- functions/begin/main.go
+++ functions/begin/main.go
@@ -1,18 +1,37 @@
 // functions/begin/main.go
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"errors"
+)
+
 // simple greet function
-//
+func greet() string {
+	return "Hello, World!"
+}
 
 // greetWithName returns a greeting with the name
-//
+func greetWithName(name string) string {
+	return "Hello, " + name + "!"
+}
 
 // greetWithName returns a greeting with the name and age of the person
 //
+func greetWithNameAndAge(name string, age int) (greeting string) {
+	greeting = "Hello, my name is " + name + " and I am " + strconv.Itoa(age) + " years old."
+	return
+}
 
 // divide divides two numbers and returns the result
 // if the second number is zero, it returns  error
-//
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Cannot divide by zero")
+	}
+	return a / b, nil
+}
 
 func main() {
 	// invoke greet function
@@ -21,9 +40,12 @@ func main() {
 	// invoke greetWithName function
 	// fmt.Println(greetWithName("Toni"))
 
+	//invoke greetWithNameAnd Age function
+	// fmt.Println(greetWithNameAndAge("Fred", 18))
+
 	// invoke divide function
-	// fmt.Println(divide(10, 2))
+	fmt.Println(divide(10, 2))
 
 	// invoke divide function with zero denominator to get an error
-	// fmt.Println(divide(5, 0))
+	fmt.Println(divide(5, 0))
 }
--- functions/functions.md
+++ functions/functions.md
@@ -4,3 +4,14 @@ Notes on packages [here](../packages/packages.md)
 
 Multi-value returns, named returns, naked returns
 - Declare functions using the `func` keyword
+- Return value is placed after the name of the function
+- Go doesn't allow overloading a function. Each function must have a unique name
+- variable type goes after the variable name
+- variables cannot be declared and given a value in the same line. 
+- Function `greetWithNameAndAge` takes in two parameters-name and age- and returns a string called greeting which is defined in the function
+  - called a named return
+  - naked return is when there is nothing following return, as shown in this function
+  - `strconv.Itoa(age)` takes the age which is an integer type and converts it to a string so it can be added to the string
+- Multi-value return is when a function can return different things and the last option is an error
+  - include the possible return types, including error as the last one
+  - if there is no error, and a valid value can be returned, put `nil` as the return for the error
